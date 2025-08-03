ac9211e19e7d2ee6e14c8a3f595b29e2f16101a9
Issam Abushaban <iba10@pitt.edu>
2025-08-03 16:29:28 -0400

    Todo List now can add items

--- todo-app/src/app/todo/todo.component.ts
+++ todo-app/src/app/todo/todo.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
+
+  showNoTextError = signal(false);
+
+  newTodo = signal('');
+  todos = signal<Todo[]>([]);
+
+  addTodo(event: Event) {
+    event.preventDefault();
+    const text = this.newTodo().trim();
+    
+    if (!text) {
+      this.showNoTextError.set(true);
+      return;
+    }
+
+    const newTask: Todo = {
+      id: Date.now(),
+      text,
+      completed: false
+    }
+
+    this.todos.update(current => [...current, newTask])
+    this.newTodo.set('');
+    this.showNoTextError.set(false); // hide error if it was showing
+    
+  }
 
 }
